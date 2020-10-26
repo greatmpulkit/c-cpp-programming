a2852328524b911c854c258bf751130c2385b2fb
pulkit bansal <greatmpulkit@gmail.com>
2020-10-26 10:59:27 +0530

    cpp

---
+++ c c++/learningcpp.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    a=3;
+    cout<<"size of int"<<sizeof(a)<<endl;
+
+    float b;
+    b=7.5;
+    cout<<"size of float"<<sizeof(b)<<endl;
+
+    char c;
+    c=a;
+    cout<<"size of char"<<sizeof(c)<<endl;
+
+    bool d;
+    d=1;
+    cout<<"size of bool"<<sizeof(d)<<endl;
+
+    short int s;
+    s=2;
+    cout<<"size of short int"<<sizeof(s)<<endl;
+
+    long int r;
+    r=12734867;
+    cout<<"size of r"<<sizeof(r)<<endl;
+
+    return 0;
+}
